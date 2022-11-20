d94ace0643acd6079cf52d3eabb7b1e32ee90919
kazuki <kazuki0819kazuki@gmail.com>
2022-11-20 16:45:15 +0900

    Unityアプリ読み込み実装

--- compornents/UnityApp.js
+++ compornents/UnityApp.js
@@ -2,10 +2,11 @@ import React from "react";
 import { Unity, useUnityContext } from "react-unity-webgl";
 export default function UnityApp() {
   const { unityProvider } = useUnityContext({
-    loaderUrl: "../build/myunityapp.loader.js",
-    dataUrl: "../build/myunityapp.data",
-    frameworkUrl: "../build/myunityapp.framework.js",
-    codeUrl: "../build/myunityapp.wasm",
+    loaderUrl: "../unity-build/Build/Build.loader.js",
+    dataUrl: "../unity-build/Build/Build.data.unityweb",
+    frameworkUrl: "../unity-build/Build/Build.framework.js.unityweb",
+    codeUrl: "../unity-build/Build/Build.wasm.unityweb",
+    streamingAssetsUrl: "../unity-build/StreamingAssets",
   });
   return (
     <Unity unityProvider={unityProvider} style={{ width: 800, height: 600 }} />
