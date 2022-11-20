fa2fb81fa9edbc09689ee4401a2a15c6bcafd275
kazuki <kazuki0819kazuki@gmail.com>
2022-11-20 13:48:13 +0900

    FirstCommit

---
+++ compornents/UnityApp.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Unity, useUnityContext } from "react-unity-webgl";
+export default function UnityApp() {
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "build/myunityapp.loader.js",
+    dataUrl: "build/myunityapp.data",
+    frameworkUrl: "build/myunityapp.framework.js",
+    codeUrl: "build/myunityapp.wasm",
+  });
+  return (
+    <Unity unityProvider={unityProvider} style={{ width: 800, height: 600 }} />
+  );
+}
